3cd6cf93a8ec838b0175f3c855422a96e2195094
Johnhyde Policharla <100892286+hyde124john@users.noreply.github.com>
2024-12-27 09:04:38 +0530

    Update and rename IsPrefixOfWord.java to IsPrefixOfWord_1455.java

--- IsPrefixOfWord_1455.java
+++ IsPrefixOfWord_1455.java
@@ -1,4 +1,4 @@
-class Solution {
+class IsPrefixOfWord_1455 {
     public int isPrefixOfWord(String sentence, String searchWord) {
         String[] words = sentence.split(" ");
         for (int i = 0; i < words.length; i++) {
